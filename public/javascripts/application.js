dd552ac5543fd959a3c5bd8076c239475841a45e
Natalia Bryzhatenko <notika314@gmail.com>
2015-03-11 14:36:37 -0400

    add comments edit, and delete routes

---
+++ public/javascripts/application.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $(".new_comment_form").submit(function(event) {
+    event.preventDefault();
+    var $target = $(event.target);
+    $.ajax({
+      type: $target.attr("method"),
+      url: "/comments/create.json",
+      data: $target.serialize(),
+      dataType: "json",
+      success: function(response) {
+              $target.prev().find("ul").append("<li class='comment'>" + response.body + "</li>");
+              },
+      error: function(response) {
+             console.log(response)
+              }
+    })
+  })
+
+});
